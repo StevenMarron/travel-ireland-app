e75853ec1a15fd65b058f9c67f9c18f8706a7348
StevenMarron <stevomarron@hotmail.com>
2022-03-30 22:06:20 +0100

    made navbar more responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,18 @@ import Ireland from '../images/Ireland-temp-logo.png'
 
 function Navbar(){
     return(
-        <nav class="navbar navbar-expand-lg">
+        <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
             <Link to="/" className="navbar-logo">
                 <img src={Ireland} className="navbar-brand" alt="Ireland Logo"></img>    
             </Link>
-            
-            <div class="container-fluid">     
-                <Link to="/" className="nav-link ms-auto">Home</Link>
+            </div>
+            <div className="container-fluid">     
+                <Link to="/" className="nav-link">Home</Link>
                 <Link to="/thingstodo" className="nav-link">Things To Do</Link>
                 <Link to="/placestostay" className="nav-link">Places To Stay</Link>                    
             </div>
+
         </nav>
 
     )
