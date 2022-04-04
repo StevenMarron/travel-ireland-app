c5c1b775bb72acf002f55e6347bdf6c51ee2a3a5
StevenMarron <stevomarron@hotmail.com>
2022-04-04 19:53:26 +0100

    added error for invalid input on activity API

--- src/components/ActivitiesAPI.js
+++ src/components/ActivitiesAPI.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import {useState} from 'react'
+import {useState,} from 'react'
 import axios from 'axios'
-import Button from 'react-bootstrap/Button'
 import Activities from '../components/Activities'
 
 function ActivitiesAPI(){
@@ -18,35 +17,35 @@ function ActivitiesAPI(){
         e.preventDefault()
         var response = await axios.get("https://failteireland.azure-api.net/opendata-api/v1/activities?$filter=address/addressRegion eq '"+query+"'")
         setActivities([response.data])
+        if([response.data.count]>0){
+            setError("")
+        }else{
+            setError(query + " is not a valid County name")
+        }
         console.log([response.data])
     }
-
-//    if (!.getActivities.length)return<h1>LOADING RESULTS..</h1>
-
+    
     return(
         <div>
             <div className="searchBar">
                 <input value={query} onChange={handleSearchCounty}/>
-                <Button className="figureButton" onClick={getActivities}>Search by County</Button>                
+                <button className="figureButton" onClick={getActivities}>Search by County</button>                
             </div>
 
+            <h1>{error}</h1>
             <div>
                 {activities.map(function(i, index){
                     return(
                         <div key={index}>
-                            <ul>
                             {i.results.map(function(j, index){
                                 return(
-                                    <li key={index}>
-                                        <Activities
+                                        <Activities key={index}
                                             name={i.results[index].name}
                                             location={i.results[index].address.addressLocality}
                                             tags={i.results[index].tags+","}
-                                        /> 
-                                    </li>
+                                        />
                                 )
                             })}
-                             </ul>
                         </div>
                     )
                 })}
