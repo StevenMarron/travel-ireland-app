d39a52dfec3c08d9e12b1dca457f9f08f6e30220
ARTOFGRAHAM <GREY.WOLF.COAT@GMAIL.com>
2022-04-07 17:27:15 +0100

    added footer and layout utpates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,20 +5,34 @@ import Ireland from '../images/Ireland-temp-logo.png';
 
 function Navbar(){
     return(
-        <nav className="navbar navbar-expand-lg">
-            <div className="container-lg">
-            <Link to="/" className="navbar-logo">
-                <img src={Ireland} className="navbar-brand" alt="Ireland Logo"></img>    
-            </Link>
-            </div>
-            <div className="container-lg">
-                <Link to="/" className="nav-link">Home</Link>
-                <Link to="/thingstodo" className="nav-link">Activities</Link>
-                <Link to="/placestostay" className="nav-link">Accommodation</Link>
-                <Link to="/covid" className="nav-link">COVID-19</Link>
-            </div>
-
-        </nav>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container-lg">
+                    <Link to="/" className="navbar-brand" tabindex="0">
+                        <img src={Ireland} className="navbar-brand" alt="Ireland Logo"></img>    
+                    </Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <Link to="/" className="nav-link" tabindex="1">Home</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link to="/thingstodo" className="nav-link" tabindex="2">Activities</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link to="/placestostay" className="nav-link" tabindex="3">Accommodation</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link to="/covid" className="nav-link" tabindex="4">COVID-19</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
     );
 }
 
