dac2ea345f4b9f7126390c28cf5e02a9a51b6eeb
Sarah O'Keeffe <x21149143@student.ncirl.ie>
2022-04-14 21:37:14 +0000

    Added Hero Image

---
+++ src/components/ActivitiesHero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ActivitiesHeroImage from '../images/Activities.png'
+
+function ActivitiesHero(){
+    return(
+        <img src={ActivitiesHeroImage}
+        className="img-fluid"
+        alt="Activities Hero Image"></img>
+    )
+}
+
+export default ActivitiesHero
--- src/views/ThingsToDo.js
+++ src/views/ThingsToDo.js
@@ -1,16 +1,22 @@
 import React from "react";
 import ActivitiesAPI from "../components/ActivitiesAPI";
 import TravelVideo from "../components/TravelVideo";
+import ActivitiesHero from "../components/ActivitiesHero.js"
 
 function ThingsToDo(){
     return(
         <div className="container">
-            <div className="col-lg-8">
-                <h1>Things to do in Ireland</h1>
-                <p>Search for activities by county</p>
-                <ActivitiesAPI />
-                <br />
-            </div>
+              <div>
+                <ActivitiesHero />
+            <div className="row">
+                <div className="col-lg-8">
+                    </div>
+                    <h1>Things to do in Ireland</h1>
+                    <p>Search for activities by county</p>
+                    <ActivitiesAPI />
+                    <br />
+                    </div>
+                </div>
             <div className="col-lg-8">
                 <h2>Need a litte inspiration for your visit?</h2>
                 <p>Watch the ten best places to visit in Ireland to help you prepare for you next trip.</p>
