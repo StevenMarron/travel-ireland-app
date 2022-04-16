4212ec1de02b245129d81f9ea233de7923d068dc
StevenMarron <stevomarron@hotmail.com>
2022-04-16 15:05:34 +0100

    applied styling to Activities API

--- src/components/ActivitiesAPI.js
+++ src/components/ActivitiesAPI.js
@@ -15,10 +15,11 @@ function ActivitiesAPI(){
         e.preventDefault()
         var response = await axios.get("https://failteireland.azure-api.net/opendata-api/v1/activities?$filter=address/addressRegion eq '" + query + "'")
         setActivities([response.data])
+        console.log([response.data])
         if([response.data.count] > 0){
             setError("");
         }else{
-            setError("'" + query + "' is not a valid county name.");
+            setError("'" + query + "' is not a valid county name. Try inputting in this format: 'Louth'");
         }
         //console.log(response);
     }
@@ -32,14 +33,18 @@ function ActivitiesAPI(){
             <div>
                 {activities.map(function(i, index){
                     return(
-                        <div key={index}>
+                        <div className="row" key={index}>
                             {i.results.map(function(j, index){
                                 return(
-                                        <Activities key={index}
+                                    <div className="col-lg-6 col-sm-12 activity-card" key={index}>
+                                        <Activities 
                                             name={i.results[index].name}
                                             location={i.results[index].address.addressLocality}
+                                            telephone={i.results[index].telephone}
+                                            website={i.results[index].url}
                                             tags={i.results[index].tags+","}
-                                        />
+                                        />                                                  
+                                    </div>
                                 );
                             })}
                         </div>
