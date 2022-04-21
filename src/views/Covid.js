994cb42374b436a6d73638037618353111416300
Sarah O'Keeffe <x21149143@student.ncirl.ie>
2022-04-21 13:49:46 +0000

    added Covid Tips

--- src/views/Covid.js
+++ src/views/Covid.js
@@ -2,6 +2,7 @@ import React from "react";
 import CovidCasesAPI from "../components/CovidCasesAPI";
 import Doyle from "../components/Doyle";
 import CovidHero from "../components/CovidHero"
+import CovidTips from "../components/CovidTips"
 
 function Covid(){
     return(
@@ -9,13 +10,19 @@ function Covid(){
             <section className="container">
                 <div className="row">
                 <div className="col-lg-8">
-                    <h1>COVID-19 cases in Ireland</h1>
-                    <p>Listed by county.</p>
+                    <h1>COVID-19 in Ireland</h1>
                     </div>
                 </div>
             </section>
             <div>
                 <CovidHero />
+            </div>
+            <div>
+                <CovidTips />
+            </div>
+            <div>
+            <h2><u>COVID-19 Cases in Ireland</u></h2>
+            <p>Listed by county:</p>
             </div>
             <section className="row">
                 <CovidCasesAPI />              
