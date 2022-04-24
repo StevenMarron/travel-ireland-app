cf641ea7f5a9c8e4092984b459b5fbb0f075448b
ARTOFGRAHAM <GREY.WOLF.COAT@GMAIL.com>
2022-04-24 22:13:51 +0100

    added content to the Accommodation page

--- src/views/PlacesToStay.js
+++ src/views/PlacesToStay.js
@@ -18,10 +18,13 @@ function PlacesToStay(){
                     <div className="row">
                         <div className="col-lg-6">
                             <h2>Where to stay in Ireland</h2>
+                            <p>Looking for accommodation during your visit to the Emerald Isle?</p>
+                            <p>Your search stops here. The 'Travel Ireland' app can help you find the right spot no matter which county you're visiting. Try it today! Enter the name of the county you're visiting and hit "Search". Our list of accommodation will guide on the 50 best places to stay. Use the number on each result as a reference when sharing. Every listing includes the name of the <em>stay</em>, its location, the type Accommodation, a web address for further information and a phone number should you wish to contact the owners.</p>
                         </div>
                     </div>
                 </section>
                 <AccommodationAPI />
+                <br />
                 {/* AccomodationAPI is imported to the PlacesToStay view component underneath a title inside a separate div */}
             </section>
         </section>
